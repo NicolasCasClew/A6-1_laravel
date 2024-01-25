85aaaf522698296fdd9cc2db905f90c8df474257
Nicolas <nicolascasclew@gmail.com>
2024-01-25 13:12:25 +0000

    all names are the same

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,11 +11,11 @@ class ProductController extends Controller
 {
     /*
     const products = [
-        ["id" => 23, "nombre" => "Luna de Pluton", "desc" => "Mi libro, Luna de Plutón, ya está disponible en todas las librerías de Argentina, Chile, Uruguay, Paraguay, Bolivia, México, Perú, Colombia, Centroamérica, España, China, Japón, la Alemania Nazi, Guatemala, Sudáfrica, Brasil, la ciudad de Bagdad, el pueblo fantasma de Pripyat, Ucrania, la Antártida, el Polo Norte, Bangladesh, Corea del Norte, Namekusei, Ciudad Gótica, el Vaticano, el imperio Romano, Chernobyl, los abismos marinos, el estado de Pensilvania, Jerusalén, la isla de Poveglia en Italia, los poblados de Pompeya y Herculano, Camboya, Egipto, el pueblo de Baviera, Rusia, Turkmenistán, Italia, Inglaterra, Irak, la antigua Persia, el trecho de las Marianas", "imgUrl" => "/img/book.png", "precio" => 20],
-        ["id" => 43, "nombre" => "Poly Station 5", "desc" => "1500 juegos preinstalados", "imgUrl" => "/img/game.png", "precio" => 150],
-        ["id" => 65, "nombre" => "Caja Débil", "desc" => "Apta para guardar objetos no deseados", "imgUrl" => "/img/safe.png", "precio" => 350],
-        ["id" => 75, "nombre" => "Daiads", "desc" => "Ole pa jugar al furbo viva el Beti", "imgUrl" => "/img/shoe.png", "precio" => 12],
-        ["id" => 88, "nombre" => "Cyclops 2", "desc" => "Perfecto para ir a ver el Titanic con 4 amigos mas", "imgUrl" => "/img/submarine.png", "precio" => 250000]
+        ["id" => 23, "nombre" => "Luna de Pluton", "descripcion" => "Mi libro, Luna de Plutón, ya está disponible en todas las librerías de Argentina, Chile, Uruguay, Paraguay, Bolivia, México, Perú, Colombia, Centroamérica, España, China, Japón, la Alemania Nazi, Guatemala, Sudáfrica, Brasil, la ciudad de Bagdad, el pueblo fantasma de Pripyat, Ucrania, la Antártida, el Polo Norte, Bangladesh, Corea del Norte, Namekusei, Ciudad Gótica, el Vaticano, el imperio Romano, Chernobyl, los abismos marinos, el estado de Pensilvania, Jerusalén, la isla de Poveglia en Italia, los poblados de Pompeya y Herculano, Camboya, Egipto, el pueblo de Baviera, Rusia, Turkmenistán, Italia, Inglaterra, Irak, la antigua Persia, el trecho de las Marianas", "url" => "/img/book.png", "precio" => 20],
+        ["id" => 43, "nombre" => "Poly Station 5", "descripcion" => "1500 juegos preinstalados", "url" => "/img/game.png", "precio" => 150],
+        ["id" => 65, "nombre" => "Caja Débil", "descripcion" => "Apta para guardar objetos no deseados", "url" => "/img/safe.png", "precio" => 350],
+        ["id" => 75, "nombre" => "Daiads", "descripcion" => "Ole pa jugar al furbo viva el Beti", "url" => "/img/shoe.png", "precio" => 12],
+        ["id" => 88, "nombre" => "Cyclops 2", "descripcion" => "Perfecto para ir a ver el Titanic con 4 amigos mas", "url" => "/img/submarine.png", "precio" => 250000]
     ];
 */
     public function index()
@@ -38,7 +38,7 @@ class ProductController extends Controller
         $viewData["id"] = $id;
         $viewData["name"] = $products["nombre"];
         $viewData["desc"] = $products["descripcion"];
-        $viewData["imgUrl"] = $products["url"];
+        $viewData["url"] = $products["url"];
 
 
         /*
@@ -46,7 +46,7 @@ class ProductController extends Controller
             if ($products[$i]["id"] == $id) {
                 $viewData["name"] = $products[$i]["nombre"];
                 $viewData["desc"] = $products[$i]["desc"];
-                $viewData["imgUrl"] = $products[$i]["imgUrl"];
+                $viewData["url"] = $products[$i]["url"];
             }
         }*/
 
