e384d9be631e62a54eb68b4016891df076ae1ab2
Nicolas <nicolascasclew@gmail.com>
2024-02-01 13:12:26 +0000

    ?

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -2,75 +2,75 @@
 @section('title', $viewData["title"])
 @section('content')
 <div class="card mb-4">
-  <div class="card-header">
-    Crear producto
-  </div>
-  <div class="card-body">
+    <div class="card-header">
+        Crear producto
+    </div>
+    <div class="card-body">
 
-    <form method="POST" action={{route('admin.product.store')}}>
-      @csrf
-      <div class="row">
-        <div class="col">
-          <div class="mb-3 row">
-            <label class="col-lg-2 col-md-6 col-sm-12 col-form-label">Nombre:</label>
-            <div class="col-lg-10 col-md-6 col-sm-12">
-              <input name="name" value="" type="text" class="form-control">
+        <form method="POST" action="{{route('admin.product.store')}}" enctype="multipart/form-data">
+            @csrf
+            <div class="row">
+                <div class="col">
+                    <div class="mb-3 row">
+                        <label class="col-lg-2 col-md-6 col-sm-12 col-form-label">Nombre:</label>
+                        <div class="col-lg-10 col-md-6 col-sm-12">
+                            <input name="name" value="" type="text" class="form-control">
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="mb-3 row">
+                        <label class="col-lg-2 col-md-6 col-sm-12 col-form-label">Precio:</label>
+                        <div class="col-lg-10 col-md-6 col-sm-12">
+                            <input name="price" value="" type="number" class="form-control">
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="mb-3 row">
-            <label class="col-lg-2 col-md-6 col-sm-12 col-form-label">Precio:</label>
-            <div class="col-lg-10 col-md-6 col-sm-12">
-              <input name="price" value="" type="number" class="form-control">
+            <div class="mb-3">
+                <label class="form-label">Descripción</label>
+                <textarea class="form-control" name="description" rows="3"></textarea>
             </div>
-          </div>
-        </div>
-      </div>
-      <div class="mb-3">
-        <label class="form-label">Descripción</label>
-        <textarea class="form-control" name="description" rows="3"></textarea>
-      </div>
-      <button type="submit" class="btn btn-primary">Enviar</button>
-    </form>
-  </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+    </div>
 </div>
 
 @if ($errors->any())
 <div class="alert alert-danger">
-  <ul>
-    @foreach ($errors->all() as $error)
-    <li>{{ $error }}</li>
-    @endforeach
-  </ul>
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
 </div>
 @endif
 
 <div class="card">
-  <div class="card-header">
-    Mantenimiento de productos
-  </div>
-  <div class="card-body">
-    <table class="table table-bordered table-striped">
-      <thead>
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Nombre</th>
-          <th scope="col">Editar</th>
-          <th scope="col">Eliminar</th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach($viewData["products"] as $product)
-        <tr>
-          <td>{{$product['id']}}</td>
-          <td>{{$product['nombre']}}</td>
-          <td><a href="#">Editar</a></td>
-          <td><a href="#">Eliminar</a></td>
-        </tr>
-        @endforeach
-      </tbody>
-    </table>
-  </div>
+    <div class="card-header">
+        Mantenimiento de productos
+    </div>
+    <div class="card-body">
+        <table class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Editar</th>
+                    <th scope="col">Eliminar</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($viewData["products"] as $product)
+                <tr>
+                    <td>{{$product['id']}}</td>
+                    <td>{{$product['nombre']}}</td>
+                    <td><a href="#">Editar</a></td>
+                    <td><a href="#">Eliminar</a></td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::get("/products", "App\Http\Controllers\ProductController@index")->name("h
 
 Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("home.product");
 
+
 Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name("admin.home.index");
 Route::get('/admin/products', 'App\Http\Controllers\Admin\AdminProductController@index')->name("admin.product.index");
-Route::post('/admin/products', 'App\Http\Controllers\Admin\AdminProductController@store')->name("admin.product.store");
+Route::post('/admin/products/store', 'App\Http\Controllers\Admin\AdminProductController@store')->name("admin.product.store");
